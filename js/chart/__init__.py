dd8b874ddf1a86c9cfbef9cf29fef0a1dfb28d11
Heberte Fernandes de Moraes <brebete@gmail.com>
2017-02-17 09:22:21 -0200

    Update chartjs to version 2.5.0

--- js/chart/__init__.py
+++ js/chart/__init__.py
@@ -3,3 +3,6 @@ from fanstatic import Library, Resource
 library = Library('chart', 'resources')
 chart_js = Resource(library, 'Chart.js',
                     minified='Chart.min.js')
+
+chart_bundle_js = Resource(library, 'Chart.bundle.js',
+                    minified='Chart.bundle.min.js')
